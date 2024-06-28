df3f03883fd44c6e9a31c66821211ddaed4f4d49
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2024-06-28 16:03:04 +0800

    Upload products data validation

--- lib/main_screens/upload_product_screen.dart
+++ lib/main_screens/upload_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradal/widgets/snack_bar.dart';
 
 class UploadProductScreen extends StatefulWidget {
   const UploadProductScreen({super.key});
@@ -8,124 +9,199 @@ class UploadProductScreen extends StatefulWidget {
 }
 
 class _UploadProductScreenState extends State<UploadProductScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldMessengerState> _scaffoldKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  late double price;
+  late int quantity;
+  late String productName;
+  late String productDescription;
+
+  void uploadProduct() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('validated');
+      print(price);
+      print(quantity);
+      print(productName);
+      print(productDescription);
+    } else {
+      MyMessageHandler.showSnackBar(
+        _scaffoldKey,
+        'Please fill all fields',
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          reverse: true,
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
+    return ScaffoldMessenger(
+      key: _scaffoldKey,
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            reverse: true,
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Container(
-                    color: Colors.blueGrey.shade100,
-                    // height: 250,
-                    // width: 390,
-                    height: MediaQuery.of(context).size.width * 0.5,
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Center(
-                      child: const Text('You have not \n \n picked images yet!',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        color: Colors.blueGrey.shade100,
+                        // height: 250,
+                        // width: 390,
+                        height: MediaQuery.of(context).size.width * 0.5,
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: Center(
+                          child: const Text(
+                            'You have not \n \n picked images yet!',
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: Divider(color: Colors.yellow, thickness: 1.5),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.50,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter price';
+                          } else if (value.isValidPrice() != true) {
+                            return 'Invalid price';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          price = double.parse(value!);
+                        },
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        //numberWithOptions(decimal: true,),
+                        decoration: textFormDecoration.copyWith(
+                          labelText: 'price',
+                          hintText: 'price \$',
+                        ),
+                      ),
+                    ),
+                  ),
+                  //todo quantity
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.50,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter quantity';
+                          } else if (value.isValidQuantity() != true) {
+                            return 'Enter a valid quantity';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          quantity = int.parse(value!);
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: textFormDecoration.copyWith(
+                          labelText: 'quantity',
+                          hintText: 'Enter Quantity',
+                        ),
+                      ),
+                    ),
+                  ),
+                  // product name:
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter name';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          productName = value!;
+                        },
+                        maxLength: 100,
+                        maxLines: 3,
+                        decoration: textFormDecoration.copyWith(
+                          labelText: 'product name',
+                          hintText: 'Enter Product name...',
+                        ),
+                      ),
+                    ),
+                  ),
+                  //! product description
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter description';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          productDescription = value!;
+                        },
+                        maxLength: 800,
+                        maxLines: 5,
+                        decoration: textFormDecoration.copyWith(
+                          labelText: 'description',
+                          hintText: 'Enter product description...',
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
-              SizedBox(
-                  height: 30,
-                  child: Divider(
-                    color: Colors.yellow,
-                    thickness: 1.5
-                  ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.50,
-                  child: TextFormField(
-                    keyboardType: TextInputType.numberWithOptions(
-                      decimal: true
-                    ),
-                    //numberWithOptions(decimal: true,),
-                    decoration: textFormDecoration.copyWith(
-                      labelText: 'price',
-                      hintText: 'price \$',
-                    ),
-                  ),
-                ),
-              ),
-              //todo quantity
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.50,
-                  child: TextFormField(
-                    keyboardType: TextInputType.number,
-                    decoration: textFormDecoration.copyWith(
-                      labelText: 'quantity',
-                      hintText: 'Enter Quantity',
-                    ),
-                  ),
-                ),
-              ),
-              // product name:
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: TextFormField(
-                    maxLength: 100,
-                    maxLines: 3,
-                    decoration: textFormDecoration.copyWith(
-                      labelText: 'product name',
-                      hintText: 'Enter Product name...',
-                    ),
-                  ),
-                ),
-              ),
-              //! product description
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: TextFormField(
-                    maxLength: 800,
-                    maxLines: 5,
-                    decoration: textFormDecoration.copyWith(
-                      labelText: 'description',
-                      hintText: 'Enter product description...',
-                    ),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
-      // buttons
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: FloatingActionButton(
-              onPressed: () {},
-              backgroundColor: Colors.yellow,
-              child: Icon(Icons.photo_library, color: Colors.black,),
+        // buttons
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: FloatingActionButton(
+                onPressed: () {},
+                backgroundColor: Colors.yellow,
+                child: Icon(
+                  Icons.photo_library,
+                  color: Colors.black,
+                ),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 8),
-            child: FloatingActionButton(
-              onPressed: () {},
-              backgroundColor: Colors.yellow,
-              child: Icon(Icons.upload, color: Colors.black,),
+            Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: FloatingActionButton(
+                onPressed: uploadProduct,
+                backgroundColor: Colors.yellow,
+                child: Icon(
+                  Icons.upload,
+                  color: Colors.black,
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -148,3 +224,16 @@ var textFormDecoration = InputDecoration(
   ),
 );
 
+// validator
+extension QuantityValidator on String {
+  bool isValidQuantity() {
+    return RegExp(r'^[1-9][0-9]*$').hasMatch(this);
+  }
+}
+
+extension PriceValidator on String {
+  bool isValidPrice() {
+    return RegExp(r'^((([1-9][0-9]*[\.]*)||([0][\.]*))([0-9]{1,2}))$')
+        .hasMatch(this);
+  }
+}
