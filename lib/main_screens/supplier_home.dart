757a9ad337cddf58134620054baa2d1b98e67cf7
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2024-07-09 18:42:44 +0800

    end of statisc

--- lib/main_screens/supplier_home.dart
+++ lib/main_screens/supplier_home.dart
@@ -1,9 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gradal/main_screens/category_screen.dart';
 import 'package:gradal/main_screens/dashboard.dart';
 import 'package:gradal/main_screens/home_screen.dart';
 import 'package:gradal/main_screens/stores.dart';
 import 'package:gradal/main_screens/upload_product_screen.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:provider/provider.dart';
+import 'package:collection/collection.dart';
+import '../providers/cart_provider.dart';
 
 class SupplierHomeScreen extends StatefulWidget {
   const SupplierHomeScreen({super.key});
@@ -21,31 +27,65 @@ class _CustomerHomeScreenState extends State<SupplierHomeScreen> {
     DashboardScreen(),
     UploadProductScreen(),
   ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _tabs[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 0,
-        type: BottomNavigationBarType.fixed,
-        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
-        selectedItemColor: Colors.black,
-        //unselectedItemColor: Colors.red,
-        currentIndex: _selectedIndex,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Category'),
-          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'Stores'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.dashboard), label: 'Dashboard'),
-          BottomNavigationBarItem(icon: Icon(Icons.upload), label: 'Upload'),
-        ],
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-      ),
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('orders')
+            .where('sid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+            .where('deliverystatus', isEqualTo: 'preparing') //preparing
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Material(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          return Scaffold(
+            body: _tabs[_selectedIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              elevation: 0,
+              type: BottomNavigationBarType.fixed,
+              selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+              selectedItemColor: Colors.black,
+              //unselectedItemColor: Colors.red,
+              currentIndex: _selectedIndex,
+              items: [
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search), label: 'Category'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.shop), label: 'Stores'),
+                BottomNavigationBarItem(
+                    icon: badges.Badge(
+                      showBadge: snapshot.data!.docs.isEmpty ? false : true,
+                      badgeStyle: badges.BadgeStyle(
+                        badgeColor: Colors.yellow,
+                        padding: EdgeInsets.all(4),
+                      ),
+                      badgeContent: Text(
+                        snapshot.data!.docs.length.toString(),
+                        style: TextStyle(fontSize: 16,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      child: Icon(Icons.dashboard,)),
+                      label: 'Dashboard'
+                    ),
+
+                    //,
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.upload), label: 'Upload'),
+              ],
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          );
+        });
   }
 }
